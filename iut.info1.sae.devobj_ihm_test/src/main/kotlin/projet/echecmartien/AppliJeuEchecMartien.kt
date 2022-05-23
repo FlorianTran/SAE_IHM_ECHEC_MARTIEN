06cd56848a5565a5336e672ff594cbbae605d464
gueguefun <43133958+gueguefun@users.noreply.github.com>
2022-05-23 12:07:07 +0200

    update

--- iut.info1.sae.devobj_ihm_test/src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ iut.info1.sae.devobj_ihm_test/src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -1,14 +1,21 @@
 package projet.echecmartien
 
 import javafx.application.Application
+import javafx.scene.Node
+import javafx.scene.Scene
+import javafx.scene.layout.GridPane
 
 import javafx.stage.Stage
 
 class AppliJeuEchecMartien: Application() {
     override fun start(primaryStage: Stage) {
 
-       TODO()
-      
+        val vue = GridPane()
+        val scene = Scene(vue ,600.0, 200.0)
+        primaryStage.title="Concateneur de chaines en javaFX"
+        primaryStage.scene=scene
+        primaryStage.show()
+
 
     }
 
